7712c4bd5fb05250cf9c5728b97f19f5391cc6d7
Connor Rowland <connor.rowland34@gmail.com>
2019-03-07 16:23:46 -0500

    Updates for demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './site-logo.png';
 import './App.css';
 const contentful = require("contentful");
 class App extends Component {
   constructor () {
     super()
     this.state = {
+      landingPageSectionOneHeroImage: "",
       landingPageSectionOneTitle: "",
-      landingPageSectionOneHeroImage: ""
+      landingSectionOneBody: "",
+      landingSectionOneSubtitle: "",
+      landingSectionTwoBody: "",
+      landingSectionTwoSubtitle: "",
+      landingSectionTwoTitle: "",
+      igPosts: [],
+      footerPromotion: false
     }
   }
 
@@ -23,25 +30,73 @@ class App extends Component {
       .getEntry("5JYEr8oVAIYyLpGrUFjxmp")
       .then(
         response => this.setState({
+          landingPageSectionOneHeroImage: response.fields.landingPageSectionOneHeroImage.fields.file.url,
           landingPageSectionOneTitle: response.fields.landingPageSectionOneTitle,
-          landingPageSectionOneHeroImage: response.fields.landingPageSectionOneHeroImage.fields.file.url
+          landingSectionOneBody: response.fields.landingSectionOneBody,
+          landingSectionOneSubtitle: response.fields.landingSectionOneSubtitle,
+          landingSectionTwoBody: response.fields.landingSectionTwoBody,
+          landingSectionTwoSubtitle: response.fields.landingSectionTwoSubtitle,
+          landingSectionTwoTitle: response.fields.landingSectionTwoTitleurl,
+          igPosts: response.fields.igPosts,
+          footerPromotion: response.fields.footerPromotion
         })
       )
       .catch(err => console.log(err));
   }
 
-  constructImageURL() {
-    var imageURL = 'https:' + this.state.landingPageSectionOneHeroImage;
+  generateIGPosts() {
+    const posts = this.state.igPosts.map((post) =>
+      <img src={post.fields.file.url} alt="IG Post" className="landing-ig--image" />
+    );
+    return posts
   }
+
   render() {
+    const { landingPageSectionOneHeroImage,
+          landingPageSectionOneTitle,
+          landingSectionOneBody,
+          landingSectionOneSubtitle,
+          landingSectionTwoBody,
+          landingSectionTwoSubtitle,
+          landingSectionTwoTitle,
+          footerPromotion } = this.state;
+
+    const posts = this.generateIGPosts();
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={this.state.landingPageSectionOneHeroImage} alt="Photo" />
-          <h1>
-            {this.state.landingPageSectionOneTitle}
-          </h1>
+        <header className="app--header">
+          <img id="logo--image" src={logo} alt="Photo" />
         </header>
+        <section id="landing--tap-into-outer">
+          <div id="landing--tap-into-module">
+            <div id="tap-into-module--image">
+              <img src={landingPageSectionOneHeroImage} alt="Photo" />
+            </div>
+            <div id="tap-into-module--text">
+              <h2>{landingPageSectionOneTitle}</h2>
+              <h3>{landingSectionOneSubtitle}</h3>
+              <p>{landingSectionOneBody}</p>
+            </div>
+          </div>
+
+          <div id="landing--instagram-photos-outer">
+            <div id="tap-instagram--text">
+              <h2>{landingSectionTwoTitle}</h2>
+              <h3>{landingSectionTwoSubtitle}</h3>
+              <p>{landingSectionTwoBody}</p>
+            </div>
+            <div id="landing--instagram-photos">
+              {posts}
+            </div>
+          </div>
+          {footerPromotion &&
+            <div id="promotion--box">
+              <h2>Explore the Latest in Luxury Living</h2>
+              <a href="mailto:" className="button">Contact a broker today</a>
+            </div>
+          }
+        </section>
       </div>
     );
   }
